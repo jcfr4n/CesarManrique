4e63c8196d363ddee3f1b862c3f4617ba58d3760
Juan Carlos Francisco Mesa <jcfm73@gmail.com>
2022-11-17 08:04:09 -0400

    finalizada la maquetación de trab unit 4  program

--- PRO/Main/src/main/Main.java
+++ PRO/Main/src/main/Main.java
@@ -1,64 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package main;
 
 import java.util.Scanner;
 
 /**
+ *
  * @author juancfm
  */
 public class Main {
 
-    /**
-     * De forma similar a la Actividad de aplicación 3.17, implementa un 
-     * algoritmo que calcule el mínimo común múltiplo de dos números dados.
-     * 
-     * Se reutilizó el código de la Actividad_3_17 y se le agregaron unas líneas 
-     * para calcular y presentar el mínimo común múltiplo.  Se procedió a 
-     * controlar el ingreso de errores por parte del usuario.
-     */
     public static void main(String[] args) {
-
-        int a, b, divisor;
-        a = b = 0;
+        double radio;
+        String option = "";
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Vamos a buscar el máximo común "
-                + "divisor y el mínimo común múltiplo de dos números dados.");
-        System.out.print("Introduzca el número A: ");
-        try {
-            a = sc.nextInt();
-        } catch (Exception ex) {
-            System.out.println("Lo siento no ha introducido un número válido.");
-            sc.close();
-            System.exit(0);
-        }
-        System.out.print("Introduzca el número B: ");
-        try {
-            b = sc.nextInt();
-        } catch (Exception ex) {
-            System.out.println("Lo siento no ha introducido un número válido.");
-            sc.close();
-            System.exit(0);
-        }
-        if (a > b) {
-            /* Cambio de una variable por otra sin auxiliar*/
-            a += b;
-            b = a - b;
-            a -= b;
+        System.out.print("Por favor indique el radio de la figura a evaluar: ");
+        radio = sc.nextDouble();
+        System.out.println("Ahora seleccione una opcion para: ");
+        System.out.println("1  Calcular la superficie o área.");
+        System.out.println("2  Calcular el volumen de la esfera.");
+        System.out.println("3  Calcular ambos.\n");
+        System.out.print("Opción: ");
+        option = sc.next("[123]");
+        System.out.println("\n");
+        switch (option) {
+            case "1" ->
+                superficie(radio);
+            case "2" ->
+                volumen(radio);
+            case "3" -> {
+                superficie(radio);
+                volumen(radio);
+            }
+
         }
 
-        divisor = a;
+    }
 
-        while (a % divisor != 0 || b % divisor != 0) {
-            divisor--;
-        }
+    /**
+     * Calcula la superficie de la circunferencia a partir de su radio
+     *
+     * @param radio radio de la circunferencia
+     */
+    static void superficie(double radio) {
+        System.out.println("El área de la circunferencia es: "
+                + (4 * Math.PI * Math.pow(radio, 2)));
+    }
 
-        System.out.println("\nEl máximo común divisor de " + a
-                + " y " + b + " es: " + divisor);
-
-        System.out.println("y el mínimo común múltiplo es: " + (a / divisor) * b);
-
-        sc.close();
-        System.out.println("Fin.");
+    /**
+     * Calcula el volumen de una esfera a partir de su radio
+     *
+     * @param radio radio de la esfera
+     */
+    static void volumen(double radio) {
+        System.out.println("El volumen de la esfera es: "
+                + ((4 * Math.PI) / 3 * Math.pow(radio, 3)));
     }
 
 }
