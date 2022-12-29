d9255236495bb128df270022836b97ecfe0c3def
Juan Carlos Francisco Mesa <jcfm73@gmail.com>
2022-12-29 23:27:34 +0000

    saving work

--- PRO/Main/src/main/Main.java
+++ PRO/Main/src/main/Main.java
@@ -4,24 +4,30 @@
  */
 package main;
 
-
 /**
  *
  * @author juancfm
  */
+
+/**
+ * Realiza la función: int[ ] buscarTodos (int t[ ], int clave), que crea y
+ * devuelve una tabla con todos los índices de los elementos donde se
+ * encuentra la clave de búsqueda. En el caso de que clave no se encuentre
+ * en la tabla t, la función devolverá una tabla vacía.
+ */
+
+/***
+ * Aprovechando la aritmética de caracteres mostraremos todos los símbolos
+ * disponibles en la codificación Unicode, comprendidos entre \u0000 y \uFFFF.
+ */
 public class Main {
-
-    /**
-     * Realiza la función: int[ ] buscarTodos (int t[ ], int clave), que crea y
-     * devuelve una tabla con todos los índices de los elementos donde se
-     * encuentra la clave de búsqueda. En el caso de que clave no se encuentre
-     * en la tabla t, la función devolverá una tabla vacía.
-     */
     public static void main(String[] args) {
-
-        System.out.println('c' + 'A' - 'a');
-
-        System.exit(0);
+        // usamos números en base hexadecimal, lo que se indica anteponiendo 0x
+        // internamente la variable codePoint contiene valores decimales
+        for (int codePoint = 0x0000; codePoint <= 0xFFFF; codePoint++) {
+            String xxxx = Integer.toHexString(codePoint); // convierte un número en su
+            // representación hexadecimal
+            System.out.println("\\u" + xxxx + ": " + (char) codePoint);
+        }
     }
-
 }
