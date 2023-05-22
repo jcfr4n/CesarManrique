d78e341c874b030717c2c33cd5add436cfd94231
Juan Carlos Francisco Mesa <jcfm73@gmail.com>
2023-05-22 21:56:08 +0100

    task 5 done

---
+++ ETS/Tema_2/Actividad_Intellij_Idea/Codigo_App/Codigo_fuente/Proyecto_1/src/Aleman.java
@@ -0,0 +1,12 @@
+
+public class Aleman {
+
+    static void imprimir(){
+
+        System.out.println("Dieses ¡Hallo Welt!");
+        System.out.println("geht auf deutsch");
+        System.out.println("\n");
+
+    }
+
+}
