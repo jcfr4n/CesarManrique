916f9146ae39b81ac04dc819e96080e132cca11d
Juan Carlos Francisco <jcfm73@gmail.com>
2023-11-18 17:43:46 +0000

    DEW_UT_07 initial commit

---
+++ DAW/2do_daw/DEW/UT_07/Tarea/provincias.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-12-2021 a las 21:09:32
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `espana`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincias`
+--
+
+CREATE TABLE `provincias` (
+  `id_comunidades` tinyint(4) NOT NULL,
+  `id_provincia` smallint(6) DEFAULT NULL,
+  `provincia` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`id_comunidades`, `id_provincia`, `provincia`) VALUES
+(8, 2, 'Albacete'),
+(10, 3, 'Alicante/Alacant'),
+(1, 4, 'Almería'),
+(16, 1, 'Araba/Álava'),
+(3, 33, 'Asturias'),
+(7, 5, 'Ávila'),
+(11, 6, 'Badajoz'),
+(4, 7, 'Balears, Illes'),
+(9, 8, 'Barcelona'),
+(16, 48, 'Bizkaia'),
+(7, 9, 'Burgos'),
+(11, 10, 'Cáceres'),
+(1, 11, 'Cádiz'),
+(6, 39, 'Cantabria'),
+(10, 12, 'Castellón/Castelló'),
+(0, 51, 'Ceuta'),
+(8, 13, 'Ciudad Real'),
+(1, 14, 'Córdoba'),
+(12, 15, 'Coruña, A'),
+(8, 16, 'Cuenca'),
+(16, 20, 'Gipuzkoa'),
+(9, 17, 'Girona'),
+(1, 18, 'Granada'),
+(8, 19, 'Guadalajara'),
+(1, 21, 'Huelva'),
+(2, 22, 'Huesca'),
+(1, 23, 'Jaén'),
+(7, 24, 'León'),
+(12, 27, 'Lugo'),
+(9, 25, 'Lleida'),
+(13, 28, 'Madrid'),
+(1, 29, 'Málaga'),
+(0, 52, 'Melilla'),
+(14, 30, 'Murcia'),
+(15, 31, 'Navarra'),
+(12, 32, 'Ourense'),
+(7, 34, 'Palencia'),
+(5, 35, 'Palmas, Las'),
+(12, 36, 'Pontevedra'),
+(17, 26, 'Rioja, La'),
+(7, 37, 'Salamanca'),
+(5, 38, 'Santa Cruz de Tenerife'),
+(7, 40, 'Segovia'),
+(1, 41, 'Sevilla'),
+(7, 42, 'Soria'),
+(9, 43, 'Tarragona'),
+(2, 44, 'Teruel'),
+(8, 45, 'Toledo'),
+(10, 46, 'Valencia/València'),
+(7, 47, 'Valladolid'),
+(7, 49, 'Zamora'),
+(2, 50, 'Zaragoza');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
